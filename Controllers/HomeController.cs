a9aa2c5d33c63781150f39c5770341fc6facc881
Neeraj Gupta - AM Account <amngupta@network.lan>
2020-07-19 01:52:05 -0600

    first cmnts

---
+++ Controllers/HomeController.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using WebApplication5.Models;
+
+namespace WebApplication5.Controllers
+{
+    public class HomeController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult About()
+        {
+            ViewData["Message"] = "Your application description page.";
+
+            return View();
+        }
+
+        public IActionResult Contact()
+        {
+            ViewData["Message"] = "Your contact page.";
+
+            return View();
+        }
+
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+
+
+
+        public IActionResult Getdata(AccountModel acc)
+        {
+            Random r = new Random();
+            int num = r.Next();
+            // string fname    = acc.fname;
+            // string lname    = acc.lname;
+            // string email    = acc.email;
+            // string mobile   = acc.mobile;
+            // string state    = acc.state;
+            // string city     = acc.city;
+            // string pincode  = acc.pincode;
+
+            ViewBag.Message1 = num;
+
+            ViewBag.Message2 = acc;
+
+            return View();
+        }
+
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
---
+++ Models/AccountModel.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace WebApplication5.Models
+{
+    public class AccountModel
+    {
+        public string fname { get; set; }
+
+        ///<summary>
+        /// Gets or sets Name.
+        ///</summary>
+        public string lname { get; set; }
+
+        ///<summary>
+        /// Gets or sets Gender.
+        ///</summary>
+        public string email { get; set; }
+
+        ///<summary>
+        /// Gets or sets City.
+        ///</summary>
+        public string mobile { get; set; }
+
+        ///<summary>
+        /// Gets or sets Gender.
+        ///</summary>
+        public string state { get; set; }
+
+        ///<summary>
+        /// Gets or sets City.
+        ///</summary>
+        public string city { get; set; }
+
+        ///<summary>
+        /// Gets or sets Gender.
+        ///</summary>
+        public string pincode { get; set; }
+
+        ///<summary>
+        /// Gets or sets City.
+        ///</summary>
+        public string password { get; set; }
+    }
+}
